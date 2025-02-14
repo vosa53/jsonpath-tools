b66ff9be0082522db2b6d3b595b6ddf0078cce27
Jan Jörka <jan.jorka@seznam.cz>
2025-02-14 16:06:06 +0100

    Added JSONPath Compliance Test Suite as a git submodule

--- src/jsonpath-tools/compliance-test-suite.spec.ts
+++ src/jsonpath-tools/compliance-test-suite.spec.ts
@@ -1,10 +1,34 @@
 import { describe, expect, it } from 'vitest';
 
-describe("test suite", () => {
-    it("is compliant with test suite", () => {
-        const parser = new JSONPathParser();
-        const result = parser.parse("$.pes.les[0, ?@.les >= 5]");
-        console.log(createSyntaxTree(result.query, true));
-        expect(5).toBe(5);
-    });
+import cts from "./jsonpath-compliance-test-suite/cts.json";
+import { JSONPathParser } from './syntax-analysis/parser';
+import { TypeChecker } from './semantic-analysis/type-checker';
+import { defaultJSONPathOptions } from './options';
+import { deepEquals } from './types';
+
+describe("JSONPath Compliance Test Suite", () => {
+    for (const test of cts.tests) {
+        it(test.name, () => {
+            const parser = new JSONPathParser();
+            const typeChecker = new TypeChecker();
+            const pathSource = test.selector;
+            const path = parser.parse(pathSource);
+            const errorsCount = path.syntaxDiagnostics.length + typeChecker.check(path, defaultJSONPathOptions).length;
+
+            if (test.invalid_selector === true) {
+                expect(errorsCount).toBeGreaterThan(0);
+            }
+            else {
+                expect(errorsCount).toBe(0);
+                if (test.document !== undefined) {
+                    const result = path.select({ rootNode: test.document, options: defaultJSONPathOptions }).nodes;
+                    if (test.results !== undefined) {
+                        expect(test.results.some(r => deepEquals(r, result))).toBeTruthy();
+                    }
+                    if (test.result !== undefined)
+                        expect(result).toEqual(test.result);
+                }
+            }
+        });
+    }
 });
