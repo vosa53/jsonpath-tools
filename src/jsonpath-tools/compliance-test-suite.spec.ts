0520d77cad96f31b09684c2a42a2c1737834f352
Jan Jörka <jan.jorka@seznam.cz>
2025-03-14 17:33:36 +0100

    Reverted compliance suite test

--- src/jsonpath-tools/compliance-test-suite.spec.ts
+++ src/jsonpath-tools/compliance-test-suite.spec.ts
@@ -7,7 +7,7 @@ import { defaultJSONPathOptions } from './options';
 import { deepEquals } from './types';
 
 describe("JSONPath Compliance Test Suite", () => {
-    /*for (const test of cts.tests) {
+    for (const test of cts.tests) {
         it(test.name, () => {
             const parser = new JSONPathParser();
             const typeChecker = new TypeChecker(defaultJSONPathOptions);
@@ -30,37 +30,5 @@ describe("JSONPath Compliance Test Suite", () => {
                 }
             }
         });
-    }*/
-    /*it("should pass", () => {
-        function validQueries() {
-            return cts.tests
-                .filter((testCase) => testCase.invalid_selector !== true)
-                .map((testCase) => {
-                    return [testCase.selector, testCase.document];
-                });
-        }
-
-        function perf(repeat: number) {
-            const parser = new JSONPathParser();
-            const checker = new TypeChecker(defaultJSONPathOptions);
-            const queries = validQueries();
-            console.log(
-                `repeating ${queries.length} queries on small datasets ${repeat} times`,
-            );
-            const start = performance.now();
-            for (let i = 0; i < repeat; i++) {
-                for (const [query, data] of queries) {
-                    const path = parser.parse(query);
-                    checker.check(path, defaultJSONPathOptions);
-                    path.select({ rootNode: data, options: defaultJSONPathOptions });
-                    //env.query(query, data);
-                    // Array.from(env.lazyQuery(query, data));
-                }
-            }
-            const stop = performance.now();
-            return (stop - start) / 1e3;
-        }
-
-        console.log(perf(10000));
-    }, 100_000);*/
+    }
 });
