0ec39fc9b353c5eb3683b38b12719c8029f54295
Jan Jörka <jan.jorka@seznam.cz>
2025-03-20 21:22:19 +0100

    Improved type annotations rendering

--- apps/jsonpath-playground/src/jsonpath-tools/editor-services/analysis-description-provider.ts
+++ apps/jsonpath-playground/src/jsonpath-tools/editor-services/analysis-description-provider.ts
@@ -8,15 +8,28 @@ export class AnalysisDescriptionProvider {
 
         if (annotations !== undefined) {
             for (const annotation of annotations) {
-                text += "\n\n---\n";
+                text += "\n\n---";
                 if (annotation.title !== "")
-                    text += `\n##### ${annotation.title}`;
+                    text += `\n\n##### ${annotation.title}`;
+                const flags: string[] = [];
+                if (annotation.deprecated) flags.push("deprecated");
+                if (annotation.readOnly) flags.push("read-only");
+                if (annotation.writeOnly) flags.push("write-only");
+                const flagsText = flags.join(", ");
+                if (flagsText !== "")
+                    text += `\n\n*${flagsText}*`;
                 if (annotation.description !== "")
-                    text += `\n${annotation.description}`;
+                    text += `\n\n${annotation.description}`;
+                if (annotation.defaultValue !== undefined) {
+                    text += "\n\n###### Default Value";
+                    text += "\n\n```json\n";
+                    text += JSON.stringify(annotation.defaultValue, null, 4);
+                    text += "\n```";
+                }
                 if (annotation.exampleValues.length > 0) {
-                    text += "\n###### Examples";
+                    text += "\n\n###### Examples";
                     for (const example of annotation.exampleValues) {
-                        text += "\n```json\n";
+                        text += "\n\n```json\n";
                         text += JSON.stringify(example, null, 4);
                         text += "\n```";
                     }
