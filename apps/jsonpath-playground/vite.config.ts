2e40be2dcc02634e68380fe713786556fc78bf5f
Jan Jörka <jan.jorka@seznam.cz>
2025-03-26 16:20:28 +0100

    Temporarily disabled PWA

--- apps/jsonpath-playground/vite.config.ts
+++ apps/jsonpath-playground/vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
-import { pwaOptions } from "./pwa-options";
+//import { VitePWA } from "vite-plugin-pwa";
+//import { pwaOptions } from "./pwa-options";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(), 
         tsconfigPaths(),
-        VitePWA(pwaOptions),
+        //VitePWA(pwaOptions),
         basicSsl()
     ],
     worker: {
