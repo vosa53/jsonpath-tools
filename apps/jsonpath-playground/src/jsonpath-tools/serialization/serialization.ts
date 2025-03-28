aaae8cdbdbebd1f9d97115d4e08a2e4def194469
Jan Jörka <jan.jorka@seznam.cz>
2025-03-28 15:22:21 +0100

    Fixed normalized path serialization

--- apps/jsonpath-playground/src/jsonpath-tools/serialization/serialization.ts
+++ apps/jsonpath-playground/src/jsonpath-tools/serialization/serialization.ts
@@ -1,23 +1,52 @@
 import { NormalizedPath } from "../normalized-path";
 
-export function serializeString(value: string): string {
-    // JSON strings are equivivalent with JSONPath strings.
-    return JSON.stringify(value);
+export function serializeString(value: string, quotes = StringQuotes.double): string {
+    let serialized = "";
+    serialized += quotes;
+    for (const character of value) {
+        if (character === "\\" || character === quotes) serialized += "\\" + character;
+        else if (character < "\u0020") {
+            if (character === "\b") serialized += "\\b";
+            else if (character === "\f") serialized += "\\f";
+            else if (character === "\n") serialized += "\\n";
+            else if (character === "\r") serialized += "\\r";
+            else if (character === "\t") serialized += "\\t";
+            else serialized += "\\u" + character.charCodeAt(0).toString(16).padStart(4, "0");
+        }
+        else serialized += character;
+    }
+    serialized += quotes;
+    return serialized;
 }
 
 export function serializeNumber(value: number): string {
-    // JSON strings are equivivalent with JSONPath strings.
-    return JSON.stringify(value);
+    return value.toString();
 }
 
 export function serializeBoolean(value: boolean) {
-    return JSON.stringify(value);
+    return value ? "true" : "false";
 }
 
-export function serializeLiteral(value: string | number | boolean | null) {
-    return JSON.stringify(value);
+export function serializeNull() {
+    return "null";
+}
+
+export function serializeLiteral(value: string | number | boolean | null, stringQuotes = StringQuotes.double) {
+    if (typeof value === "string")
+        return serializeString(value, stringQuotes);
+    else if (typeof value === "number")
+        return serializeNumber(value);
+    else if (typeof value === "boolean")
+        return serializeBoolean(value);
+    else
+        return serializeNull();
 }
 
 export function serializedNormalizedPath(path: NormalizedPath): string {
-    return "$" + path.map(ps => `[${JSON.stringify(ps)}]`).join("");
+    return "$" + path.map(ps => `[${serializeLiteral(ps, StringQuotes.single)}]`).join("");
+}
+
+export enum StringQuotes {
+    single = "'",
+    double = "\""
 }
