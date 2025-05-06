40c33e179083e6e1c59fb7c3380f0cb5441a0097
Jan Jörka <jan.jorka@seznam.cz>
2025-05-06 14:22:05 +0200

    Enabled PWA again

--- apps/jsonpath-playground/vite.config.ts
+++ apps/jsonpath-playground/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-//import { VitePWA } from "vite-plugin-pwa";
-//import { pwaOptions } from "./pwa-options";
+import { VitePWA } from "vite-plugin-pwa";
+import { pwaOptions } from "./pwa-options";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        //VitePWA(pwaOptions),
+        VitePWA(pwaOptions),
         basicSsl()
     ],
     server: {
